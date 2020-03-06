b5464b2095a5be61c82f78d9aadab5bd93e73e82
mithun@mobilefringe.com @ CodeCloud <mithun@mobilefringe.com @ CodeCloud>
2020-03-06 13:30:36 -0500

    Long Beach Test Site 1.0  /main.js

--- main.js
+++ main.js
@@ -185,7 +185,7 @@ require(['Vue', 'vuex', 'vue2-filters', 'vue_router', 'routes', 'vuex-router-syn
         methods: {
             loadData: async function() {
                 try{
-                    await this.$store.dispatch('initializeApi', { site: "twinpine", version: "v4" });
+                    await this.$store.dispatch('initializeApi', { site: "longbeach", version: "v4" });
                     await Promise.all([this.$store.dispatch("getData", "property")]);
                     this.property.mm_host = this.property.mm_host.replace("http:", "");
                     let results = await Promise.all([this.$store.dispatch("INITIALIZE_LOCALE"), this.$store.dispatch("getData", "hours"), this.$store.dispatch("getData", "stores")]);
